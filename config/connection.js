55636030e3be18e52446fd793d2deae2503ff7f5
Saleema Ali <sali1390@gmail.com>
2017-02-08 23:28:33 -0500

    Removed leftover connection.js file

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-var Sequelize = require('sequelize');
-
-var sequelize = new Sequelize('cl702kb9icfpzwxt', 'tt18wa40ohx2ft2z', 'hxpd6o1g5zizlir4', {
-    host: 'ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql'
-})
-
-module.exports = sequelize;
